d30af333aa362cffd774e8560e8a6784674ec8e7
Zhao1zh <xiaozhaolife@gmail.com>
2025-03-14 02:45:27 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "赵一峙博客空间";
+export const SITE_DESCRIPTION = "欢迎来访我的博客!";
